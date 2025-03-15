69f81ba1aa92fe84f7ddba9d504338a49edad582
Chugunov Roman <Zebs-BMK@yandex.ru>
2025-03-15 12:57:49 +0300

    xrPhysics/PHSimpleCharacterInline.h: validate obj != nullptr

--- src/xrPhysics/PHSimpleCharacterInline.h
+++ src/xrPhysics/PHSimpleCharacterInline.h
@@ -125,7 +125,7 @@ void CPHSimpleCharacter::UpdateDynamicDamage(dContact* c, u16 obj_material_idx, 
         IPhysicsShellHolder* object = bo1 ? retrieveRefObject(c->geom.g2) : retrieveRefObject(c->geom.g1);
         IPhysicsShellHolder* obj = smart_cast<IPhysicsShellHolder*>(object);
         VERIFY(obj);
-        if (!obj->ObjectGetDestroy())
+        if (obj && !obj->ObjectGetDestroy())
         {
             m_collision_damage_info.m_contact_velocity = c_vel;
             m_collision_damage_info.m_dmc_signum = bo1 ? 1.f : -1.f;
