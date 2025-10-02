32428ebad26458dc561508ef0c8788d8b9c6a99a
ChugunovRoman <Zebs-BMK@yandex.ru>
2025-10-02 15:19:09 +0300

    xrAICore/Components/problem_solver_inline.h: return first operator if id is -1

--- src/xrAICore/Components/problem_solver_inline.h
+++ src/xrAICore/Components/problem_solver_inline.h
@@ -348,6 +348,8 @@ TEMPLATE_SPECIALIZATION
 IC _operator_ptr CProblemSolverAbstract::get_operator(const _operator_id_type& operator_id)
 {
     typename OPERATOR_VECTOR::iterator I = std::lower_bound(m_operators.begin(), m_operators.end(), operator_id);
+    if (m_operators.end() == I)
+        return m_operators.at(0).get_operator();
     R_ASSERT2(m_operators.end() != I, make_string("m_operators.size=[%d], operator_id=[%d]", m_operators.size(), operator_id));
     return ((*I).get_operator());
 }
